2736bbc4792d17e0823c73d39b49e562fa643c86
Illedrac <huntermillspg@gmail.com>
2023-10-20 15:07:42 -0400

    Added Initial RGB Implementation -- Very Bad

--- SortingAlgs/src/SortingAlgorithms.cpp
+++ SortingAlgs/src/SortingAlgorithms.cpp
@@ -25,31 +25,12 @@ public:
     {
         maxNumberValue = SCREEN_WIDTH_HEIGHT;
         maxNumbers = SCREEN_WIDTH_HEIGHT;
-
+    
         createUnsortedArray();
 
-        DrawArray();
-
+        //DrawArray();
+        DrawColorfullArray();
         return true;
-    }
-
-    void createUnsortedArray() {
-        std::mt19937 gen(rd()); // mersenne_twister_engine seeded with rd()
-        std::uniform_int_distribution<> distrib(1, maxNumberValue);
-
-        unsortedNumbers.clear();
-        unsortedNumbersArrSize = (sizeof(unsortedNumbersArr) / sizeof(int));
-
-        int index = 0;
-        while (unsortedNumbers.size() < maxNumbers) {
-            int curNum = distrib(gen);
-
-            if (unsortedNumbers.find(curNum) == unsortedNumbers.end()) {
-                unsortedNumbers.insert(curNum);
-                unsortedNumbersArr[index] = curNum;
-                index++;
-            }
-        }
     }
 
     bool OnUserUpdate(float fElapsedTime) override
@@ -57,8 +38,14 @@ public:
         if (GetKey(olc::Key::ESCAPE).bPressed)
             return false;
 
-        bubbleSort();
-
+        if (GetKey(olc::Key::SPACE).bPressed)
+            shouldContinue = !shouldContinue;
+        
+        //std::this_thread::sleep_for(std::chrono::milliseconds{ 50 });
+        if (shouldContinue) {
+            runBubbleSort();
+            //runInsertionSort();
+        }
             
         //}
         //DrawArray();
@@ -66,47 +53,83 @@ public:
         return true;
     }
 
-    void bubbleSort() {
+    // needs to start as curI = 1 curJ = 0
+    void runInsertionSort() {
+        if (startSort) {
+            curI = 1;
+            curJ = curI - 1;
+            insertionSortTempValue = unsortedNumbersArr[curI];
+            startSort = false;
+        }
+        
+
+        if (curI < unsortedNumbersArrSize) {        // for curI = 1; i 
+            if (curJ >= 0 && unsortedNumbersArr[curJ] > insertionSortTempValue) {
+                unsortedNumbersArr[curJ + 1] = unsortedNumbersArr[curJ];
+                //DrawArray(curJ + 1);
+                //DrawArray(curJ);
+                DrawColorfullArray(curJ + 1);
+                DrawColorfullArray(curJ);
+
+                curJ--;
+            }
+            else {
+                unsortedNumbersArr[curJ + 1] = insertionSortTempValue;
+                //DrawArray(curJ + 1);
+                DrawColorfullArray(curJ + 1);
+                curI++;
+                insertionSortTempValue = unsortedNumbersArr[curI];
+                curJ = curI - 1;
+            }
+           
+        }
+    }
+    
+    //needs to start as curI == curJ == 0
+    void runBubbleSort() {
         if (curI < unsortedNumbersArrSize) {
             if (curJ < unsortedNumbersArrSize) {
-                bubbleSort(curI, curJ);
+                bubbleSort();
                 curJ++;
             }
             else if (curJ >= unsortedNumbersArrSize) {
                 curI++;
                 curJ = curI + 1;
-                bubbleSort(curI, curJ);
+                bubbleSort();
             }
         }
     }
 
-    void bubbleSort(int i, int j) {
+    void bubbleSort() {
 
-        if (i < unsortedNumbersArrSize) {
+        if (curI < unsortedNumbersArrSize) {
 
-            if (unsortedNumbersArr[j] < unsortedNumbersArr[i]) {
-                int temp = unsortedNumbersArr[i];
-                unsortedNumbersArr[i] = unsortedNumbersArr[j];
-                unsortedNumbersArr[j] = temp;
+            if (unsortedNumbersArr[curJ] < unsortedNumbersArr[curI]) {
+                int temp = unsortedNumbersArr[curI];
+                unsortedNumbersArr[curI] = unsortedNumbersArr[curJ];
+                unsortedNumbersArr[curJ] = temp;
 
-                DrawArray(i);
-                DrawArray(j);
+                //DrawArray(curI);
+                //DrawArray(curJ);
+                DrawColorfullArray(curI);
+                DrawColorfullArray(curJ);
+
             }
         }
-        /*
-        for (int i = 0; i < ( sizeof(unsortedNumbersArr) / sizeof(int) ); i++) {
-            for (int j = i + 1; j < ( sizeof(unsortedNumbersArr) / sizeof(int) ); j++)
-            {
-                if (unsortedNumbersArr[j] < unsortedNumbersArr[i]) {
-                    int temp = unsortedNumbersArr[i];
-                    unsortedNumbersArr[i] = unsortedNumbersArr[j];
-                    unsortedNumbersArr[j] = temp;
-                    DrawArray(i, olc::CYAN);
-                }
-                //std::this_thread::sleep_for(std::chrono::milliseconds{ 2 });
-            }
+    }
+
+    void DrawColorfullArray(int x) {
+        //DrawLine(x, SCREEN_WIDTH_HEIGHT, x, maxNumberValue - unsortedNumbersArr[x], olc::Pixel(255%unsortedNumbersArr[x], unsortedNumbersArr[x]*2 % 255, unsortedNumbersArr[x]*3 % 255));
+        DrawLine(x, SCREEN_WIDTH_HEIGHT, x, maxNumberValue - unsortedNumbersArr[x], unsortedNumbersArrColors[x]);
+        DrawLine(x, maxNumberValue - unsortedNumbersArr[x], x, 0, olc::BLACK);
+    }
+
+    void DrawColorfullArray() {
+        FillRect(0, 0, ScreenHeight(), ScreenWidth(), olc::BLACK);
+        for (int x = 0; x < SCREEN_WIDTH_HEIGHT; x++) {
+            //DrawLine(x, SCREEN_WIDTH_HEIGHT, x, maxNumberValue - unsortedNumbersArr[x], olc::Pixel(unsortedNumbersArr[x] % 255, unsortedNumbersArr[x]*2 % 255, unsortedNumbersArr[x]*3 % 255));
+            DrawLine(x, SCREEN_WIDTH_HEIGHT, x, maxNumberValue - unsortedNumbersArr[x], unsortedNumbersArrColors[x]);
         }
-        */
     }
 
     void DrawArray(int x) {
@@ -126,19 +149,62 @@ public:
         }
     }
     
+    void createUnsortedArray() {
+        std::mt19937 gen(rd()); // mersenne_twister_engine seeded with rd()
+        std::uniform_int_distribution<> distrib(1, maxNumberValue);
+
+        unsortedNumbers.clear();
+        unsortedNumbersArrSize = (sizeof(unsortedNumbersArr) / sizeof(int));
+
+        int index = 0;
+        while (unsortedNumbers.size() < maxNumbers) {
+            if (updateRGB) {
+                updateRGB = false;
+                if (b <= 255 && r >= 1 && g >= 1) {
+
+                    if (r < 255 && b == 1) r++;
+                    if (g < 255 && r >= 254 && b <= 2) g++;
+                    if (b < 255 && g >= 254) { b++; r--; }
+                    if (r <= 2 && b >= 254) { g--; }
+                }
+            }
+            else {
+                updateRGB = true;
+            }
+
+            int curNum = distrib(gen);
+
+            if (unsortedNumbers.find(curNum) == unsortedNumbers.end()) {
+                unsortedNumbers.insert(curNum);
+                unsortedNumbersArr[index] = curNum;
+                unsortedNumbersArrColors[index] = olc::Pixel(r, g, b);
+                index++;
+            }
+        }
+    }
 
     std::random_device rd;  // a seed source for the random number engine
     std::mt19937 gen; // mersenne_twister_engine seeded with rd()
     std::uniform_int_distribution<> distrib;
 
     std::unordered_set<int> unsortedNumbers;
+
+    bool startSort = true;
+    bool shouldContinue = false;
+
     int maxNumberValue;
     int maxNumbers;
     int unsortedNumbersArr[SCREEN_WIDTH_HEIGHT];
+    olc::Pixel unsortedNumbersArrColors[SCREEN_WIDTH_HEIGHT];
     int unsortedNumbersArrSize;
-    bool shouldUpdate = true;
     int curI = 0;
     int curJ = 0;
+    int insertionSortTempValue;
+    int r = 255;
+    int g = 40;
+    int b = 1;
+    bool updateRGB = true;
+
 };
 
 
